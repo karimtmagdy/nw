936ff56d9321d08d2c3e74c9ffaaa717ef867929
karimtmagdy <karimtmagdy@gmail.com>
2025-04-27 19:34:14 +0300

    create method sign up

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { login } = require("../services/auth.service");
-const { validateLogin } = require("../validator/auth.validate");
+const { login, register } = require("../services/auth.service");
+const {
+  validateLogin,
+  validateRegister,
+} = require("../validator/auth.validate");
 
+router.post("/sign-up", validateRegister, register);
 router.post("/sign-in", validateLogin, login);
 
 module.exports = router;
