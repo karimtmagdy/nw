f726675596d911135b6bcdc1690974a04bbf79bb
karimtmagdy <karimtmagdy@gmail.com>
2025-04-27 19:27:18 +0300

    update cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { errorHandler, AppError } = require("./api/middleware/errorHandler");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
